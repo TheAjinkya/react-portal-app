2da6c74c8a25c743e7bf976ea643f827779aa5c9
unknown <ajinkya.chanshetty@gmail.com>
2023-03-12 23:57:04 +0000

    Updated with layout changes

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = ()=>{
+
+    return(
+        <div className='ui fixed menu'>
+            <div className='ui container center'>
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    )
+
+}
+
+export default Contact
